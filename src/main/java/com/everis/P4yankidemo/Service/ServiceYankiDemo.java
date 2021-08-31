ef47d00ade690d62451075f81d9144ae226d7a65
EVERIS-KEV <kpalmall@everis.com>
2021-08-30 23:54:26 -0500

    Update 3.75

--- src/main/java/com/everis/P4yankidemo/Service/ServiceYankiDemo.java
+++ src/main/java/com/everis/P4yankidemo/Service/ServiceYankiDemo.java
@@ -14,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
-@Slf4j
 @Service
 public class ServiceYankiDemo {
 
